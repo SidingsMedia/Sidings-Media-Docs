6d5b6cac0ec45b00388bd1d67d9b67375f1407ec
Matthew Nickson <mnickson@sidingsmedia.com>
2021-08-31 20:29:01 +0100

    Added author
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,2 +1,18 @@
+import re
+
+project = u'Sidings Media Documentation'
+slug = re.sub(r'\W+', '-', project.lower())
 author = u'Sidings Media'
 copyright = author
+
+latex_documents = [
+  ('index', '{0}.tex'.format(slug), project, author, 'manual'),
+]
+
+man_pages = [
+    ('index', slug, project, [author], 1)
+]
+
+texinfo_documents = [
+  ('index', slug, project, author, slug, project, 'Miscellaneous'),
+]
